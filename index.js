56f2e29318427a0d1cc6297168dc96e72280b898
frunezshyna <fsdcayanan.student@ua.edu.ph>
2024-11-20 13:44:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,41 +11,28 @@
 
 //npm i jsonwebtoken
 
-
 // Server Setup
 const express = require("express");
+
 // const port = 4000;
 const mongoose = require("mongoose")
 require("dotenv").config();
 
 const app = express();
 
-
-
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const userRoutes = require("./routes/User-Routes.js");
-
-//Backend Routes
+const courseRoutes = require("../routes/Course-Routes.js");
 
 //MongoDB Connection
 mongoose.connect(process.env.MONGODB_STRING);
 mongoose.connection.once("open",() => console.log(`Now connected to MongoDB Atlas`));
 
-
 //Backend Routes
 app.use("/users", userRoutes);
+app.use("/courses", courseRoutes);
 
 app.listen(process.env.PORT || 3000, () => console.log(`API is now connected on port ${process.env.PORT || 3000}`));
-
-
-//User Post
-// localhost:4000/users/register
-
-//User Login
-//localhost:4000/users/login
-
-//check email
-//localhost:4000/user/check-email
